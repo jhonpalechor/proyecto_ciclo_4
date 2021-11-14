ce4468870efb0e1d6e62484d517376ff8b349717
jcarito <86927292+jcarito@users.noreply.github.com>
2021-11-14 15:25:34 -0500

    se realiza cambios en el login

---
+++ empleado_front/src/components/login/login.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import axios from 'axios';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import {APIHOST as host } from '../app.json';
+import './login.css';
+import {isNull} from 'util';
+import Cookies from 'universal-cookie';
+import { calculaEspiracionSession } from '../helper/helper';
+import Loading from '../loading/loading';
+
+const cookies = new Cookies();
+
+export default class login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Loading: false,
+            usuario:'',
+            pass:'',
+    };
+    }
+    iniciarSession(){
+
+        this.setState({ loading: true});
+
+        axios.post(`${host}/usuarios/login`,{
+            usuario: this.state.usuario,
+            pass: this.state.pass,
+            })
+            .then((response) => {
+                if (isNull(response.data.token)) {
+                    alert('Usuario y/o contraseña invalidas');
+                } else {
+                    cookies.set('_s', response.data.token, {
+                        path:'/',
+                        expires: calculaEspiracionSession(),
+                    });
+                }
+                this.setState({ loading: false});
+            })
+            .cath(error => {
+                console.log(error);
+                this.setState({ loading: false});
+            });
+    }
+    render () {
+        return (
+    <Container id="login-container">
+            <Loading show={this.state.loading} />
+        <Row>
+            <Col>
+                <Row>
+                    <h2>Iniciar Sessión</h2>
+                </Row>
+                <Row>
+                    <Col
+                    sm="12"
+                    xs="12"
+                    md={{ span: 4, offset: 4 }}
+                    lg={{ span: 4, offset: 4 }}
+                    xl={{ span: 4, offset: 4 }}
+                    >
+            <Form>
+                <Form.Group>
+                    <Form.Label >Usuario</Form.Label>
+                    <Form.Control  
+                    onChange={(e) => 
+                    
+                    this.setState({usuario:e.target.value})}
+
+                    ></Form.Control>
+                    
+                </Form.Group>
+        
+        <Form.Group>
+            <Form.Label >contraseña</Form.Label>
+            <Form.Control 
+            type="password"
+            onChange={(e) =>
+                this.setState({ pass: e.target.value })
+            }
+            />
+        </Form.Group>
+        <Button 
+        variant="primary" 
+        type="submit" 
+            onCLick={() => {
+                this.iniciarSession();
+            }}
+            >
+                        Iniciar Session
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Col>
+    </Row>         
+    </Container>
+        );
+        }
+    }
